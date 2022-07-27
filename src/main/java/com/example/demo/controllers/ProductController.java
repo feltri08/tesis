52a059ab99e2b8f6fb0fd7a8779c8ed808a4c1db
Felipe <feltri08@gmail.com>
2022-07-27 11:43:42 -0500

    Api

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 @CrossOrigin
 public class ProductController {
 
@@ -22,7 +22,7 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @GetMapping("/products")
+    @GetMapping()
     public List<Product> getPatients(){
         return productRepository.findAll();
     }
