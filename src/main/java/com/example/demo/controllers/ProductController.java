53f82300d22fc565b546b291f959840a7dae1b86
Felipe <feltri08@gmail.com>
2022-07-27 11:37:37 -0500

    Api

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.entities.Product;
 import com.example.demo.repos.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
     private ProductRepository productRepository;
